edea33d2ffbd051bd2c8395843ff2582c6ab00eb
blacksoulgem95 <11537307+blacksoulgem95@users.noreply.github.com>
2022-04-01 18:34:59 +0200

    linux_release

--- linux/release.sh
+++ linux/release.sh
@@ -1,9 +1,9 @@
 mv build/linux/x64/release/bundle/csv_generator build/linux/x64/release/bundle/lib/csv_generator
-cp linux/*.desktop build/linux/x64/release/bundle/
-cp linux/csv_generator.*.sh build/linux/x64/release/bundle/
-cp linux/install-*.sh build/linux/x64/release/bundle/
-cp linux/csv_generator build/linux/x64/release/bundle/
-cp README.md build/linux/x64/release/bundle/
-cp linux/csv_generator.png build/linux/x64/release/bundle/
-cp linux/libCwtch.so build/linux/x64/release/bundle/lib/
-cp linux/tor build/linux/x64/release/bundle/lib
+cp linux/*.desktop build/linux/x64/release/bundle/ 2>/dev/null || :
+cp linux/csv_generator.*.sh build/linux/x64/release/bundle/ 2>/dev/null || :
+cp linux/install-*.sh build/linux/x64/release/bundle/ 2>/dev/null || :
+cp linux/csv_generator build/linux/x64/release/bundle/ 2>/dev/null || :
+cp README.md build/linux/x64/release/bundle/ 2>/dev/null || :
+cp linux/csv_generator.png build/linux/x64/release/bundle/ 2>/dev/null || :
+cp linux/libCwtch.so build/linux/x64/release/bundle/lib/ 2>/dev/null || :
+cp linux/tor build/linux/x64/release/bundle/lib 2>/dev/null || :
